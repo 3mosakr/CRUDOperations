5ba8eed52949c9b426bd941fe1077a05b7cf80ce
Mahmoud Sakr <sakr364@outlook.com>
2024-12-30 23:14:36 +0200

    Add project files.

---
+++ CRUDOperations/Controllers/ProductsController.cs
@@ -0,0 +1,76 @@
+﻿using CRUDOperations.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDOperations.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public ProductsController(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            var records = _dbcontext.Set<Product>().ToList(); 
+            return Ok(records);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Product> GetById(int id)
+        {
+            var record = _dbcontext.Set<Product>().Find(id);
+            if(record is null) 
+                return NotFound();
+
+            return Ok(record);
+        }
+
+        [HttpPost]
+        [Route("")] // default post Method
+        public ActionResult<int> CreateProduct(Product product)
+        {
+            product.Id = 0;  // defult value use for insert in db - can remove it
+            _dbcontext.Set<Product>().Add(product);
+
+            _dbcontext.SaveChanges();
+            return Ok(product.Id);
+        }
+
+        [HttpPut]
+        [Route("")] 
+        public ActionResult<int> UpdateProduct(Product product)
+        {
+            var existingProduct = _dbcontext.Set<Product>().Find(product.Id);
+            if (existingProduct == null)
+                return NotFound();
+            existingProduct.Name = product.Name;
+            existingProduct.Sku = product.Sku;
+
+            _dbcontext.Set<Product>().Update(existingProduct);
+            _dbcontext.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]  // must send id
+        public ActionResult<int> DeleteProduct(int id)
+        {
+            var existingProduct = _dbcontext.Set<Product>().Find(id);
+            if (existingProduct == null)
+                return NotFound();
+
+            _dbcontext.Set<Product>().Remove(existingProduct);
+            _dbcontext.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ CRUDOperations/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDOperations.Data
+{ 
+    public class ApplicationDbContext : DbContext
+    {
+        
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            //optionsBuilder.UseSqlServer("Data Source=DESKTOP-03FOKSV;Database=products;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False")
+            //    .LogTo(Console.WriteLine);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .ToTable("Products");
+        }
+    }
+}
